da7cc86b0f8c5a88b596bef0481812a18ad7e3e7
avamsy <avamsykrishna9@gmail.com>
2024-11-18 13:11:41 +0530

    index,navbar setup done

---
+++ src/main/java/com/ecom/controllers/HomeController.java
@@ -0,0 +1,27 @@
+package com.ecom.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String indexPage(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
+    }
+    
+    
+}
