cd9d426ebc8116b007d4c978989dc13375f04f21
avamsy <avamsykrishna9@gmail.com>
2024-11-19 13:53:41 +0530

    Category edit & delete Implement

--- src/main/java/com/ecom/controllers/AdminController.java
+++ src/main/java/com/ecom/controllers/AdminController.java
@@ -10,10 +10,12 @@ import java.nio.file.StandardCopyOption;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -29,7 +31,7 @@ public class AdminController {
 
     @Autowired
     private CategoryService categoryService;
-    
+
     @GetMapping("/")
     public String adminIndexPage() {
         return "admin/index";
@@ -41,7 +43,9 @@ public class AdminController {
     }
 
     @GetMapping("/category")
-    public String categoryPage() {
+    public String categoryPage(Model model) {
+
+        model.addAttribute("categorys", categoryService.getAllCategory());
         return "admin/category";
     }
 
@@ -62,7 +66,7 @@ public class AdminController {
             Category saveCategory = categoryService.saveCategory(category);
 
             if (ObjectUtils.isEmpty(saveCategory)) {
-                
+
                 session.setAttribute("errorMsg", "Not saved ! internal server error");
             } else {
 
@@ -72,14 +76,70 @@ public class AdminController {
                         + file.getOriginalFilename());
 
                 // System.out.println(path);
-                
+
                 Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
 
                 session.setAttribute("successMsg", "Saved successfully");
             }
         }
+        return "redirect:/admin/category";
+    }
+
+    @GetMapping("/deleteCategory/{id}")
+    public String deleteCategory(@PathVariable int id, HttpSession session) {
+        Boolean deleteCategory = categoryService.deleteCategory(id);
+
+        if (deleteCategory) {
+            session.setAttribute("successMsg", "category deleted successfully");
+        } else {
+            session.setAttribute("errorMsg", "something wrong on server");
+        }
 
         return "redirect:/admin/category";
     }
+
+    @GetMapping("/loadEditCategory/{id}")
+    public String loadEditCategory(@PathVariable int id, Model model) {
+
+        model.addAttribute("category", categoryService.getCategoryById(id));
+        return "admin/edit_category";
+    }
+
+    @PostMapping("/updateCategory")
+	public String updateCategory(@ModelAttribute Category category, @RequestParam("file") MultipartFile file,
+			HttpSession session) throws IOException {
+
+		Category oldCategory = categoryService.getCategoryById(category.getId());
+
+		String imageName = file.isEmpty() ? oldCategory.getImageName() : file.getOriginalFilename();
+
+		if (!ObjectUtils.isEmpty(category)) {
+
+			oldCategory.setName(category.getName());
+			oldCategory.setIsActive(category.getIsActive());
+			oldCategory.setImageName(imageName);
+		}
+
+		Category updateCategory = categoryService.saveCategory(oldCategory);
+
+		if (!ObjectUtils.isEmpty(updateCategory)) {
+
+			if (!file.isEmpty()) {
+				File saveFile = new ClassPathResource("static/img").getFile();
+
+				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "category_img" + File.separator
+						+ file.getOriginalFilename());
+
+				// System.out.println(path);
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+			}
+
+			session.setAttribute("successMsg", "Category update successfully");
+		} else {
+			session.setAttribute("errorMsg", "something wrong on server");
+		}
+
+		return "redirect:/admin/loadEditCategory/" + category.getId();
+	}
 
 }
